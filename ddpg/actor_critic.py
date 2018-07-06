1fb6de6a1d3cff391c4ac950734c137bcbea4c98
Madhavun <madvncv@gmail.com>
2018-07-06 13:36:58 -0400

    more comments

--- ddpg/actor_critic.py
+++ ddpg/actor_critic.py
@@ -11,17 +11,20 @@ class ActorCritic:
                   action_dims,
                   ):
 
+        # placeholders for observation and action
         self.obs_pl = tf.placeholder(tf.float32, shape=(None,obs_dims), name='obs_pl')
         self.action_pl = tf.placeholder(tf.float32, shape=(None, action_dims), name='action_pl')
 
+        # designing the actor network
         with tf.variable_scope('actor') as vs:
             # Weight initializers - layer 1
             magnitude = np.sqrt(float(obs_dims))
             initializers = [tf.random_uniform_initializer(minval=-1/magnitude,maxval=1/magnitude)]
-            # hidden layers
+            # Weight initializers - hidden layers
             magnitude = np.sqrt(float(hidden_size))
             initializers += [tf.random_uniform_initializer(minval=-1/magnitude,maxval=1/magnitude)]*num_layers
 
+            # creating a feed-forward neural network with observation as input and tanh activated action_dim outputs
             self.actor = tf.tanh(neural_network(self.obs_pl,
                                             [hidden_size]*num_layers + [action_dims],
                                             name='actor',
@@ -33,6 +36,10 @@ class ActorCritic:
             # Weight initializers - layer 1
             magnitude = np.sqrt(float(obs_dims))
             initializer = tf.random_uniform_initializer(minval=-1/magnitude,maxval=1/magnitude)
+
+            # creating just one layer of the critic first with
+                #1. obsevation as input and
+                #2. batch_normalization
             self.critic_lr1 = tf.nn.relu(tf.layers.batch_normalization(
                                             tf.layers.dense(inputs=self.obs_pl,
                                                 units=hidden_size,
@@ -41,14 +48,16 @@ class ActorCritic:
                                                 name='critic_obs_layer',
                                                 )))
 
-            # hidden layers
+            # Weight initializers - hidden layers
             magnitude = np.sqrt(float(hidden_size))
             initializers = [tf.random_uniform_initializer(minval=-1/magnitude,maxval=1/magnitude)]*(num_layers)
+
+            # concatenating the action input to the output of layer 1 critic as input to layer 2 of critic
             self.critic = neural_network(tf.concat([self.critic_lr1,self.action_pl],1),
                                             [hidden_size]*(num_layers-1) + [1],
                                             name='critic',
                                             reuse=None,
                                             initializers=initializers
                                             )
-            # to train actor
+            # partial derivative of critic with respect to actions - to train the actor
             self.actor_training_signal = tf.gradients(self.critic, self.action_pl, stop_gradients=[self.obs_pl])
--- ddpg/ddpg.py
+++ ddpg/ddpg.py
@@ -30,7 +30,7 @@ class DDPG:
 
         # creating networks
         with tf.variable_scope('ddpg'):
-            # main actor-critic
+            # creating the main actor and main critic networks
             with tf.variable_scope('main') as vs:
                 self.main = ActorCritic(hidden_size,
                                               num_layers,
@@ -39,7 +39,7 @@ class DDPG:
                                               )
                 vs.reuse_variables()
 
-            # target actor-critic
+            # creating the target actor and target critic networks
             with tf.variable_scope('target') as vs:
                 self.target = ActorCritic(hidden_size,
                                               num_layers,
@@ -48,7 +48,7 @@ class DDPG:
                                               )
                 vs.reuse_variables()
 
-        # get trainable variables
+        # get trainable variables from all 4 networks
         self.main_params = self._params('ddpg/main')
         self.target_params = self._params('ddpg/target')
         self.main_actor_params = self._params('ddpg/main/actor')
@@ -65,34 +65,56 @@ class DDPG:
         self.inverted_done_batch = tf.cast(tf.logical_not(tf.cast(self.done_batch, tf.bool)), tf.float32)'''
 
         with tf.name_scope('train_main'):
+            # train main critic
             with tf.name_scope('critic'):
-                # train main critic
+                # placeholders for rewards and not(done) from minibatch sampled from ReplayBuffer
                 self.rewards_pl = tf.placeholder(tf.float32, shape=[1,None], name='rewards_pl')
                 self.inverted_done_pl = tf.placeholder(tf.float32, shape=[None,1], name='inverted_done_pl')
+
+                # desired output of main critic Q(s_t) = r + gamma * target_critic(s_t+1)
+                # target_critic is computed using s_t+1 and target_actor(s_t+1)
+                # note that s_t and s_t+1 come from minibatch sampled from ReplayBuffer
                 self.main_critic_desired = tf.transpose(self.rewards_pl) + tf.multiply(self.inverted_done_pl, self.gamma*(self.target.critic))
+
+                # loss is simply root mean squared error of desired and actual output
+                # actual output of main_critic is computed from s_t and actions from sampled minibatch as opposed to main_actor(s_t)
                 self.main_critic_loss = tf.losses.mean_squared_error(self.main_critic_desired, self.main.critic)
                 self.main_critic_optimizer = tf.train.AdamOptimizer(self.critic_lr).minimize(self.main_critic_loss)
 
+            # train main actor - gradient provided by critic
             with tf.name_scope('actor'):
-                # train main actor - gradient provided by critic
+                # placeholder for incoming gradient from critic i.e. gradient of critic w.r.t. action
                 self.main_action_gradient_pl = tf.placeholder(tf.float32, shape=[None,self.action_dims], name='action_gradient_pl')
+
+                # find gradient of action w.r.t. to actor_parameters with incoming gradients from critic as initial value
+                # note minus sign in incoming gradients from critic
                 self.combined_gradients = tf.gradients(self.main.actor, self.main_actor_params, -self.main_action_gradient_pl)
+
+                # averaging across gradients from sampled minibatch
                 self.main_actor_gradients = [tf.div(self.combined_gradients[i],self.batch_size) for i in range(len(self.combined_gradients))]
+
+                # applying gradients to actor parameters
                 self.main_actor_optimizer = tf.train.AdamOptimizer(self.actor_lr).apply_gradients(zip(
                                                                                                self.main_actor_gradients,
                                                                                                self.main_actor_params))
 
-        # update target nets
+        # initialize target actor and critic networks to be same as main actor and critic networks
         self.init_target_params = [tf.assign(self.target_params[i], self.main_params[i]) for i in range(len(self.main_params))]
+
         with tf.name_scope('train_target'):
-            self.update_target_actor_params = [self.target_actor_params[i].assign(tf.multiply(self.main_actor_params[i], self.tau)+
-                                                                            tf.multiply(self.target_actor_params[i], 1.-self.tau)
-                                                                            )
-                                                    for i in range(len(self.target_actor_params))]
+            # updating parameters of target critic
+            # target_critic_params <- (1-tau)*target_critic_params + tau*main_critic_params
             self.update_target_critic_params = [self.target_critic_params[i].assign(tf.multiply(self.main_critic_params[i], self.tau)+
                                                                             tf.multiply(self.target_critic_params[i], 1.-self.tau)
                                                                             )
                                                     for i in range(len(self.target_critic_params))]
+
+            # updating target actor params
+            # target_actor_params <- (1-tau)*target_actor_params + tau*main_actor_params
+            self.update_target_actor_params = [self.target_actor_params[i].assign(tf.multiply(self.main_actor_params[i], self.tau)+
+                                                                            tf.multiply(self.target_actor_params[i], 1.-self.tau)
+                                                                            )
+                                                    for i in range(len(self.target_actor_params))]
 
         # housekeeping
         self.vars_inited = False
@@ -101,20 +123,24 @@ class DDPG:
 
     def act(self, sess, obs):
         ''' actor output for one observation'''
+        # initializing variables only once
         if not self.vars_inited:
             sess.run(tf.global_variables_initializer())
             sess.run(self.init_target_params)
             self.vars_inited = True
 
+        # just get output of main actor given the observation, s
         return sess.run(self.main.actor,feed_dict={self.main.obs_pl: np.reshape(obs, [-1, self.obs_dims])})
 
     def act_critc(self, sess, obs):
         ''' actor and critic output for observation'''
+        # initializing variables only once
         if not self.vars_inited:
             sess.run(tf.global_variables_initializer())
             sess.run(self.init_target_params)
             self.vars_inited = True
 
+        # get output of main actor and main critic given an observation
         actor_out = sess.run(self.main.actor, feed_dict={self.main.obs_pl: np.reshape(obs, [-1, self.obs_dims])})
         critic_out = sess.run(self.main.critic,feed_dict={self.main.obs_pl: np.reshape(obs, [-1, self.obs_dims]),
                                                            self.main.action_pl: actor_out})
@@ -123,59 +149,76 @@ class DDPG:
 
     def update_target_critic(self, sess):
         ''' run op to update target critic'''
+        # initializing variables only once
         if not self.vars_inited:
             sess.run(tf.global_variables_initializer())
             sess.run(self.init_target_params)
             self.vars_inited = True
 
+        # update target critic parameters
         sess.run(self.update_target_critic_params)
 
     def update_target_actor(self, sess):
         ''' run op to update target actor'''
+        # initializing variables only once
         if not self.vars_inited:
             sess.run(tf.global_variables_initializer())
             sess.run(self.init_target_params)
             self.vars_inited = True
 
+        # update target actor parameters
         sess.run(self.update_target_actor_params)
 
     def train_critic(self, sess):
         ''' update critic and return action gradients to update actor'''
+        # initializing variables only once
         if not self.vars_inited:
             sess.run(tf.global_variables_initializer())
             sess.run(self.init_target_params)
             self.vars_inited = True
 
+        # train main critic using a minibatch sampled from ReplayBuffer
         if self.buffer.size() >= self.batch_size:
             batch = self.buffer.sample_batch(self.batch_size)
             batch = self._preprocess_batch(batch)
 
-            #print('\n\n',np.shape(batch['next_s']),'\n\n')
-            #desired_critic_out = sess.run(self.main_critic_desired, feed_dict={self.target.obs_pl: batch['s']})
-            target_act = sess.run(self.target.actor, feed_dict={self.target.obs_pl: batch['next_s']})
+            # items used:
+                #1. target_actor_output for s_t+1 in minibatch
+                #2. target_critic output for s_t+1 and target_actor_output from 1
+                #3. main_critic_output using s_t and a_t from minibatch
+                #4. rewards from minibatch
+                #5. not(done from minibatch)
+            #
+            target_act = sess.run(self.target.actor, feed_dict={self.target.obs_pl: batch['next_s']}) #1 above
             _, loss, summary = sess.run([self.main_critic_optimizer, self.main_critic_loss, self.merged_summaries], feed_dict={
-                                                        self.target.obs_pl: batch['next_s'],
-                                                        self.target.action_pl: target_act,
-                                                        self.main.obs_pl: batch['s'],
-                                                        self.rewards_pl: batch['r'],
-                                                        self.main.action_pl: batch['a'],
-                                                        self.inverted_done_pl: np.transpose(np.logical_not(batch['t']).astype('float32'))
+                                                        self.target.obs_pl: batch['next_s'],          #2
+                                                        self.target.action_pl: target_act,            #2
+                                                        self.main.obs_pl: batch['s'],                 #3
+                                                        self.main.action_pl: batch['a'],              #3
+                                                        self.rewards_pl: batch['r'],                  #4
+                                                        self.inverted_done_pl: np.transpose(np.logical_not(batch['t']).astype('float32')) #5
                                                         })
             return loss,summary
         else:
-            return 100.
+            return 100. # NOT SURE OF THIS IS OKAY
 
     def train_actor(self, sess):
         ''' update main actor params'''
+        # initializing variables only once
         if not self.vars_inited:
             sess.run(tf.global_variables_initializer())
             sess.run(self.init_target_params)
             self.vars_inited = True
 
+        # train main actor using a minibatch sampled from ReplayBuffer
+        # this is a different sample from what was used to train the critic - hope that's okay
         if self.buffer.size() >= self.batch_size:
             batch = self.buffer.sample_batch(self.batch_size)
             batch = self._preprocess_batch(batch)
 
+            # items used:
+                #1. main_actor_output using s_t from sampled minibatch
+                #2. action_gradients from main_critic given 1. and s_t from minibatch
             main_batch_actions = sess.run(self.main.actor, feed_dict={self.main.obs_pl: batch['s']})
             main_action_grad = sess.run(self.main.actor_training_signal, feed_dict={self.main.obs_pl: batch['s'],
                                                                                     self.main.action_pl: main_batch_actions})[0]
